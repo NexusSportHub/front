f3b1262396cab23b458ea627cd2ca20e7ec3541c
Natalia Domínguez <ndcarbajosa@gmail.com>
2024-02-02 18:05:58 +0100

    creacion de la parte visual de la parte primera del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,15 @@ import Dashboard from './components/Dashboard';
 import Metodos from './components/Metodos';
 import Periodos from './components/Periodos';
 import Suscripcion from './components/Suscribcion';
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import miImagen from './assets/images/nexus-sport-hub-logo.jpg';
 function App() {
+
   const [decodedUserData, setDecodedUserData] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -27,6 +34,7 @@ function App() {
     setIsLoggedIn(true);
   };
 
+  const defaultTheme = createTheme();
   const handleLogoutClick = () => {
     // Call the Google logout function
     googleLogout();
@@ -38,28 +46,74 @@ function App() {
 
   return (
     <>
-      <GoogleLogin
-        clientId={import.meta.env.VITE_CLIENT_ID}
-        onSuccess={handleLoginSuccess}
-        onError={() => {
-          console.log('Login Failed');
-        }}
-        useOneTap
-      />
 
-      {decodedUserData ? (
+
+      <ThemeProvider theme={defaultTheme} >
+        <Grid container component="main" sx={{ height: '100vh', width: '100vw' }}>
+          <CssBaseline />
+          <Grid item xs={12} sm={12} md={5} component={Paper} elevation={6} square>
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >
+              <GoogleLogin
+                clientId={import.meta.env.VITE_CLIENT_ID}
+                onSuccess={handleLoginSuccess}
+                onError={() => {
+                  console.log('Login Failed');
+                }}
+                useOneTap
+              />
+
+
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={12} // Ajusta el tamaño de la primera columna para que ocupe todo el ancho
+            sm={12} // Ajusta el tamaño de la primera columna para que ocupe todo el ancho
+            md={7}
+            sx={{
+              backgroundImage: `url(${miImagen})`,
+              // object_fit: cover,
+              backgroundRepeat: 'no-repeat',
+              backgroundColor: (t) =>
+                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+              backgroundSize: 'fill',
+              backgroundPosition: 'center',
+              display: 'flex', // Añade esta línea
+              alignItems: 'center', // Añade esta línea
+            }}
+          />
+        </Grid>
+        {decodedUserData ? (
+          <div>
+            <UserDataTable userData={decodedUserData} />
+            <button onClick={handleLogoutClick}>Logout</button>
+          </div>
+        ) : null}
+        {isLoggedIn && <Dashboard />}
+        {isLoggedIn && <StyledTableCell />}
+        {isLoggedIn && <SportsButton />}
+        {isLoggedIn && <Metodos />}
+        {isLoggedIn && <Periodos />}
+        {isLoggedIn && <Suscripcion />}
+      </ThemeProvider>
+
+
+      {/* {decodedUserData ? (
         <div>
           <UserDataTable userData={decodedUserData} />
           <button onClick={handleLogoutClick}>Logout</button>
         </div>
-      ) : null}
+      ) : null} */}
 
-      {isLoggedIn && <Dashboard />}
-      {isLoggedIn && <StyledTableCell />}
-      {isLoggedIn && <SportsButton />}
-      {isLoggedIn && <Metodos />}
-      {isLoggedIn && <Periodos />}
-      {isLoggedIn && <Suscripcion />}
 
     </>
   );
